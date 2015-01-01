5e5d365d62fb0c6cc140f3d7a5738f84637d2c3a
Lolzen <funer@gmx.at>
2015-01-01 14:34:09 +0000

    Revert "broken fixes"
    This reverts commit 192e7e55ae26b5902af343caa6457d55cd218265.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1363,6 +1363,8 @@ struct task_struct {
 #endif
 	unsigned long atomic_flags; /* Flags needing atomic access. */
 
+	unsigned long atomic_flags; /* Flags needing atomic access. */
+
 	pid_t pid;
 	pid_t tgid;
 
